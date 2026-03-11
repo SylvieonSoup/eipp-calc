a6603825dd3132a4e42631ebc6408319df8e4655
Celestia74 <77862536+Celestia74@users.noreply.github.com>
2026-03-11 11:33:26 -0500

    shared_template edits and themes update

--- src/js/theme-toggle.js
+++ src/js/theme-toggle.js
@@ -15,14 +15,23 @@
 */
 var prefersDarkTheme = localStorage.getItem('darkTheme') || '';
 var darkThemeButton = document.getElementById('dark-theme-toggle');
+var prefersClassicTheme = localStorage.getItem('classicTheme');
+var classicThemeButton = document.getElementById('classic-theme-toggle');
 darkThemeButton.value = prefersDarkTheme;
+classicThemeButton.value = prefersClassicTheme;
 updateTheme();
 
 function updateTheme() {
 	var isDark = prefersDarkTheme ? prefersDarkTheme === 'true' : window.matchMedia('(prefers-color-scheme: dark)').matches;
-
+	var isClassic = prefersClassicTheme ? prefersClassicTheme === 'true' : false;
+	var mainStyles = document.getElementById('main-styles');
 	var darkStyles = document.getElementById('dark-theme-styles');
-	darkStyles.disabled = !isDark;
+	var classicStyles = document.getElementById('classic-styles');
+	var classicDarkStyles = document.getElementById('classic-dark-styles');
+	classicStyles.disabled = !isClassic;
+	mainStyles.disabled = isClassic;
+	classicDarkStyles.disabled = (!isDark || !isClassic);
+	darkStyles.disabled = (!isDark || isClassic);
 }
 
 /*
@@ -31,10 +40,13 @@ function updateTheme() {
 */
 function toggleTheme() {
 	prefersDarkTheme = document.getElementById('dark-theme-toggle').value;
+	prefersClassicTheme = document.getElementById('classic-theme-toggle').value;
 	updateTheme();
 
 	localStorage.setItem('darkTheme', prefersDarkTheme);
+	localStorage.setItem('classicTheme', prefersClassicTheme);
 }
 
 darkThemeButton.addEventListener('change', toggleTheme);
+classicThemeButton.addEventListener('change', toggleTheme);
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
